701c293e465332efbff28d3689a71eb6e1c352e2
korges <korgesik@gmail.com>
2025-06-29 10:18:29 +0200

    Prettier fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 function App() {
-
-  return (
-    <div className="min-h-screen bg-gray-900 text-gray-100">
-      <div className="max-w-screen-lg m-auto pt-16">
-        <p>Hello World</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-gray-900 text-gray-100">
+            <div className="max-w-screen-lg m-auto pt-16">
+                <p>Hello World</p>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,14 +1,14 @@
-export const TESTNET_PACKAGE_ID = "";
-export const DEVNET_PACKAGE_ID = "";
-export const LOCAL_PACKAGE_ID = "";
-export const MAINNET_PACKAGE_ID = "";
+export const TESTNET_PACKAGE_ID = ""
+export const DEVNET_PACKAGE_ID = ""
+export const LOCAL_PACKAGE_ID = ""
+export const MAINNET_PACKAGE_ID = ""
 
-export const TESTNET_ADMIN_CAP = "";
-export const DEVNET_ADMIN_CAP = "";
-export const LOCAL_ADMIN_CAP = "";
-export const MAINNET_ADMIN_CAP = "";
+export const TESTNET_ADMIN_CAP = ""
+export const DEVNET_ADMIN_CAP = ""
+export const LOCAL_ADMIN_CAP = ""
+export const MAINNET_ADMIN_CAP = ""
 
-export const TESTNET_DASHBOARD_ID = "";
-export const DEVNET_DASHBOARD_ID = "";
-export const LOCAL_DASHBOARD_ID = "";
-export const MAINNET_DASHBOARD_ID = "";
+export const TESTNET_DASHBOARD_ID = ""
+export const DEVNET_DASHBOARD_ID = ""
+export const LOCAL_DASHBOARD_ID = ""
+export const MAINNET_DASHBOARD_ID = ""
