394d16ad5dd5690a14f683064848af9d2142935e
Korges <korgesik@gmail.com>
2025-06-29 09:53:44 +0200

    --amend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,10 @@ import viteLogo from "/vite.svg";
 function App() {
 
   return (
-    <div className="min-h-screen bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
-      <div className="max-w-screen-xl m-auto pt-16">
-        Hello World XD
+    <div className="min-h-screen bg-gray-900 text-gray-100">
+      <div className="max-w-screen-lg m-auto pt-16">
+        <p>Hello World</p>
       </div>
-      
     </div>
   );
 }
