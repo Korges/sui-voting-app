5e0648e03e9c4d43ab3d4f01fd64ee1b7c0bf033
korges <korgesik@gmail.com>
2025-07-27 09:20:54 +0200

    MintModal updates

--- frontend/src/components/moment/MintModal.tsx
+++ frontend/src/components/moment/MintModal.tsx
@@ -16,15 +16,48 @@ export const MintModal = ({
     };
 
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50" onClick={handleOverlayClick}>
-            <div className="bg-white dark:bg-gray-800 p-6 rounded-lg max-w-md w-full">
-                <p>body</p>
-                <button
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center" onClick={handleOverlayClick}>
+            <div className=" bg-black p-6 rounded-lg max-w-md w-full border border-gray-600">
+                <div className="flex justify-between space-x-4 text-2xl text-white font-bold mb-6">
+                    <div>Confirm Mint</div>
+                    <div>
+                        <button
+                        onClick={onClose}
+                        className="px-2 bg-transparent">
+                            x
+                        </button>
+                    </div>
+                </div>
+                <div className="rounded-lg bg-gray-600 text-gray-400 p-3">
+                    <div className="flex justify-end space-x-4">
+                        <div className="">MINT FEE</div>
+                        <div className="">-</div>
+                        <div className="">SUI</div>
+                    </div>
+                    <div className="flex justify-end space-x-4">
+                        <div className="">PROTOCOL FEE</div>
+                        <div className="">0.01</div>
+                        <div className="">SUI</div>
+                    </div>
+                    <div className="flex justify-end space-x-4">
+                        <div className="">NETWORK FEE</div>
+                        <div className="">0.01</div>
+                        <div className="">SUI</div>
+                    </div>
+                </div>
+                <div className="flex justify-end space-x-4 text-gray-400 my-6">
+                    <div>TOTAL</div>
+                    <div>0.11</div>
+                    <div>SUI</div>
+                </div>
+                <div>
+                    <button
                     onClick={onClose}
                     className="w-full border border-gray-300 dark:border-gray-600 px-4 py-2 rounded hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors"
-                >
-                Mint
-                </button>
+                    >
+                    Mint
+                    </button>
+                </div>
           </div>
         </div>
     );
